6baa226b363da2bd698d6b46f9cbda433300f1e0
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-07-23 12:29:22 +0200

    add a UserButton to the SetupPage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default function SetupPage() {
   return (
     <div className="p-4">
-      <p>E-commerce Admin Dashboard</p>
-      <Button size="default" variant="outline">
-        Click me!
-      </Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
