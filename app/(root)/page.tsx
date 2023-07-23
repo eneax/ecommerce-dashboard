b689cd11b2c69b0bb9073e0b710879414889ba49
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-07-23 11:44:13 +0200

    experiment with Button from shadcn-ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <p>E-commerce Admin Dashboard</p>;
+  return (
+    <div className="p-4">
+      <p>E-commerce Admin Dashboard</p>
+      <Button size="default" variant="outline">
+        Click me!
+      </Button>
+    </div>
+  );
 }
