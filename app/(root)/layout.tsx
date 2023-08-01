4aabd200bddc81eb3b9bc812f51955cf483ab466
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-08-01 19:47:09 +0200

    create root layout

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,30 @@
+import { auth } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
+
+import prismaDb from "@/lib/prisma-db"
+
+export default async function SetupLayout({ children }: {
+  children: React.ReactNode
+}) {
+  const { userId } = auth()
+
+  if (!userId) {
+    redirect('/sign-in')
+  }
+
+  const store = await prismaDb.store.findFirst({
+    where: {
+      userId
+    }
+  })
+
+  if (store) {
+    redirect(`/${store.id}`)
+  }
+
+  return (
+    <>
+      {children}
+    </>
+  )
+}
