14baf27f78190f470cc37e75449645b25831e666
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-08-01 19:46:37 +0200

    implement dashboard layout

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,33 @@
+import { redirect } from 'next/navigation'
+import { auth } from "@clerk/nextjs"
+
+import prismaDb from '@/lib/prisma-db'
+
+export default async function DashboardLayout({ children, params }: {
+  children: React.ReactNode
+  params: { storeId: string }
+}) {
+  const { userId } = auth()
+
+  if (!userId) {
+    redirect('/sign-in')
+  }
+
+  const store = await prismaDb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId
+    }
+  })
+
+  if (!store) {
+    redirect('/')
+  }
+
+  return (
+    <>
+      <p>This will be a navbar</p>
+      {children}
+    </>
+  )
+}
