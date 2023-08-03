09f71478faab14f028ab161beb7d1428238e1f73
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-08-03 21:24:37 +0200

    implement store switcher functionality

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,28 @@
-import { UserButton } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import { UserButton, auth } from '@clerk/nextjs'
 
+import prismaDb from '@/lib/prisma-db'
+
+import { StoreSwitcher } from '@/components/store-switcher'
 import { NavbarRoutes } from '@/components/navbar-routes'
 
-export const Navbar = () => {
+export const Navbar = async () => {
+  const { userId } = auth()
+
+  if (!userId) {
+    redirect('/sign-in')
+  }
+
+  const stores = await prismaDb.store.findMany({
+    where: {
+      userId,
+    },
+  })
+
   return (
     <div className='border-b'>
       <div className="flex items-center h-16 px-4">
-        <div>store switcher</div>
+        <StoreSwitcher items={stores} />
         <NavbarRoutes className='mx-6' />
         <div className="flex items-center space-x-4 ml-auto">
           <UserButton afterSignOutUrl='/' />
