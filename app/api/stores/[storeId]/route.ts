bf0a685b04e531dc8f2a7dcb61855dd2de0c2dc5
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-08-04 17:35:51 +0200

    implement store DELETE route API

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -40,3 +40,31 @@ export async function PATCH(
   }
 }
 
+export async function DELETE(
+  _req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store ID is required", { status: 400 });
+    }
+
+    const store = await prismaDb.store.deleteMany({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log("[STORE_DELETE_ERROR]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
