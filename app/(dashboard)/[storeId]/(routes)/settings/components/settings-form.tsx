4ce259170a626cb3cf1f95b893469f73811ba05c
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-08-04 17:09:13 +0200

    create UI to update name in SettingsForm

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,28 +1,95 @@
 "use client";
 
+import * as React from "react";
+import * as z from "zod";
 import { Trash } from "lucide-react";
-
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Store } from "@prisma/client";
 
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { Heading } from "@/components/heading";
 
 interface SettingsFormProps {
   initialData: Store;
 }
 
+const settingsFormSchema = z.object({
+  name: z.string().min(1),
+});
+
+type SettingsFormValues = z.infer<typeof settingsFormSchema>;
+
 export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const [open, setOpen] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  const form = useForm<SettingsFormValues>({
+    resolver: zodResolver(settingsFormSchema),
+    defaultValues: initialData,
+  });
+
+  const onSubmit = async (values: SettingsFormValues) => {
+    console.log(values);
+  };
+
   return (
     <>
       <div className="flex items-center justify-between">
         <Heading title="Settings" description="Manage store preferences" />
-        <Button variant="destructive" size="icon" onClick={() => {}}>
-          <Trash className="w-4 h-4" />
+        <Button
+          disabled={loading}
+          variant="destructive"
+          size="sm"
+          onClick={() => {}}
+        >
+          <Trash className="mr-2 w-4 h-4" />
+          <span>Delete store</span>
         </Button>
       </div>
 
       <Separator />
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full space-y-8"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={loading}
+                      placeholder="Store name"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <Button disabled={loading} type="submit">
+            Save
+          </Button>
+        </form>
+      </Form>
     </>
   );
 };
