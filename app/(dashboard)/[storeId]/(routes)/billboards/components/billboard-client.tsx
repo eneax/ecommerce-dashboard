ed2f57eb3a5cd3980b397bc182c250773a14068b
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-08-06 11:14:53 +0200

    create a basic Billboard page

--- app/(dashboard)/[storeId]/(routes)/billboards/components/billboard-client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/billboard-client.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useRouter, useParams } from "next/navigation";
 import { Plus } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
@@ -7,6 +8,9 @@ import { Separator } from "@/components/ui/separator";
 import { Heading } from "@/components/heading";
 
 export function BillboardClient() {
+  const router = useRouter();
+  const params = useParams();
+
   return (
     <>
       <div className="flex items-center justify-between">
@@ -14,7 +18,9 @@ export function BillboardClient() {
           title="Billboards (0)"
           description="Manage your store billboards"
         />
-        <Button>
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
           <Plus className="mr-2 w-4 h-4" />
           Create Billboard
         </Button>
