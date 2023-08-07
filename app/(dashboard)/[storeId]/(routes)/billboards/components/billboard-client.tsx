47875c543d2ae53ddb5f217c0999bf5dc6c8158d
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-08-07 20:36:16 +0200

    fetch billboards in dedicated page

--- app/(dashboard)/[storeId]/(routes)/billboards/components/billboard-client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/billboard-client.tsx
@@ -2,12 +2,17 @@
 
 import { useRouter, useParams } from "next/navigation";
 import { Plus } from "lucide-react";
+import { Billboard } from "@prisma/client";
 
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Heading } from "@/components/heading";
 
-export function BillboardClient() {
+interface BillboardClientProps {
+  data: Billboard[];
+}
+
+export function BillboardClient({ data }: BillboardClientProps) {
   const router = useRouter();
   const params = useParams();
 
@@ -15,7 +20,7 @@ export function BillboardClient() {
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title="Billboards (0)"
+          title={`Billboards (${data.length})`}
           description="Manage your store billboards"
         />
         <Button
--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,13 +1,30 @@
+import prismaDb from "@/lib/prisma-db";
+
 import { BillboardClient } from "./components/billboard-client";
 
-const Billboards = () => {
+const BillboardsPage = async ({
+  params,
+}: {
+  params: {
+    storeId: string;
+  };
+}) => {
+  const billboards = await prismaDb.billboard.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient />
+        <BillboardClient data={billboards} />
       </div>
     </div>
   );
 };
 
-export default Billboards;
+export default BillboardsPage;
