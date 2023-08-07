e73a5c83fcb80bd31a9b526d5f34f8a578804a2e
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-08-07 20:53:19 +0200

    format billboards using date-fns

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,6 +1,8 @@
+import { format } from "date-fns";
 import prismaDb from "@/lib/prisma-db";
 
 import { BillboardClient } from "./components/billboard-client";
+import { BillboardColumn } from "./components/columns";
 
 const BillboardsPage = async ({
   params,
@@ -18,10 +20,18 @@ const BillboardsPage = async ({
     },
   });
 
+  const formattedBillboards: BillboardColumn[] = billboards.map(
+    (billboard) => ({
+      id: billboard.id,
+      label: billboard.label,
+      createdAt: format(billboard.createdAt, "MMMM do, yyyy"),
+    })
+  );
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient data={billboards} />
+        <BillboardClient data={formattedBillboards} />
       </div>
     </div>
   );
