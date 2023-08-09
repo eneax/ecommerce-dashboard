82e55d8a26d9904522ced7f9ebcc24f020ede1d7
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-08-09 20:24:35 +0200

    fix authMiddleware public route

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/api/:path/*"],
+  publicRoutes: ["/api/:path*"],
 });
 
 export const config = {
