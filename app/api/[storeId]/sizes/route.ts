94c422c5c18e5f2e8caa275eb348e9545cc2f366
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-08-11 20:44:37 +0200

    implement sizes API routes

---
+++ app/api/[storeId]/sizes/route.ts
@@ -0,0 +1,75 @@
+import prismaDb from "@/lib/prisma-db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  _req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    if (!params.storeId) {
+      return new NextResponse("Store ID is required", { status: 400 });
+    }
+
+    const sizes = await prismaDb.size.findMany({
+      where: {
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(sizes);
+  } catch (error) {
+    console.log("[SIZES_GET_ERROR]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { name, value } = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!value) {
+      return new NextResponse("Value is required", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store ID is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismaDb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const size = await prismaDb.size.create({
+      data: {
+        name,
+        value,
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(size);
+  } catch (error) {
+    console.log("[SIZES_POST_ERROR]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
