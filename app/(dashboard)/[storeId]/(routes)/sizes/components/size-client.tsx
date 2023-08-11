e92cfbc1ab4b4c185a662b536f000103367e7a2f
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-08-11 20:43:29 +0200

    create sizes page and related components

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/size-client.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useRouter, useParams } from "next/navigation";
+import { Plus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Heading } from "@/components/heading";
+import { DataTable } from "@/components/data-table";
+import { ApiList } from "@/components/api-list";
+
+import { SizeColumn, columns } from "./columns";
+
+interface SizeClientProps {
+  data: SizeColumn[];
+}
+
+export function SizeClient({ data }: SizeClientProps) {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Sizes (${data.length})`}
+          description="Manage the store sizes"
+        />
+        <Button onClick={() => router.push(`/${params.storeId}/sizes/new`)}>
+          <Plus className="mr-2 w-4 h-4" />
+          Create Size
+        </Button>
+      </div>
+
+      <Separator />
+
+      <DataTable searchKey="name" columns={columns} data={data} />
+
+      <Heading title="API" description="API calls for Sizes" />
+
+      <Separator />
+
+      <ApiList entityName="sizes" entityIdName="sizeId" />
+    </>
+  );
+}
---
+++ app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -0,0 +1,39 @@
+import { format } from "date-fns";
+import prismaDb from "@/lib/prisma-db";
+
+import { SizeClient } from "./components/size-client";
+import { SizeColumn } from "./components/columns";
+
+const SizesPage = async ({
+  params,
+}: {
+  params: {
+    storeId: string;
+  };
+}) => {
+  const sizes = await prismaDb.size.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedSizes: SizeColumn[] = sizes.map((size) => ({
+    id: size.id,
+    name: size.name,
+    value: size.value,
+    createdAt: format(size.createdAt, "MMMM do, yyyy"),
+  }));
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SizeClient data={formattedSizes} />
+      </div>
+    </div>
+  );
+};
+
+export default SizesPage;
