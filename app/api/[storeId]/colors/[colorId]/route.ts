1b1bc20d05822fb3547a33a786d4eed5af564a42
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-08-12 11:15:43 +0200

    implement colorId API routes

---
+++ app/api/[storeId]/colors/[colorId]/route.ts
@@ -0,0 +1,117 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
+
+import prismaDb from "@/lib/prisma-db";
+
+export async function GET(
+  _req: Request,
+  { params }: { params: { colorId: string } }
+) {
+  try {
+    if (!params.colorId) {
+      return new NextResponse("Color ID is required", { status: 400 });
+    }
+
+    const color = await prismaDb.color.findUnique({
+      where: {
+        id: params.colorId,
+      },
+    });
+
+    return NextResponse.json(color);
+  } catch (error) {
+    console.log("[COLOR_GET_ERROR]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string; colorId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { name, value } = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!value) {
+      return new NextResponse("Value is required", { status: 400 });
+    }
+
+    if (!params.colorId) {
+      return new NextResponse("Color ID is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismaDb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const color = await prismaDb.color.updateMany({
+      where: {
+        id: params.colorId,
+      },
+      data: {
+        name,
+        value,
+      },
+    });
+
+    return NextResponse.json(color);
+  } catch (error) {
+    console.log("[COLOR_PATCH_ERROR]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  _req: Request,
+  { params }: { params: { storeId: string; colorId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    if (!params.colorId) {
+      return new NextResponse("Color ID is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismaDb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const color = await prismaDb.color.deleteMany({
+      where: {
+        id: params.colorId,
+      },
+    });
+
+    return NextResponse.json(color);
+  } catch (error) {
+    console.log("[COLOR_DELETE_ERROR]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
