a0ecb2e299f00f55419e7e542c40a8045a3d8315
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-08-13 12:39:37 +0200

    implement productId API routes

---
+++ app/api/[storeId]/products/[productId]/route.ts
@@ -0,0 +1,169 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
+
+import prismaDb from "@/lib/prisma-db";
+
+export async function GET(
+  _req: Request,
+  { params }: { params: { productId: string } }
+) {
+  try {
+    if (!params.productId) {
+      return new NextResponse("Product ID is required", { status: 400 });
+    }
+
+    const product = await prismaDb.product.findUnique({
+      where: {
+        id: params.productId,
+      },
+      include: {
+        images: true,
+        category: true,
+        size: true,
+        color: true,
+      },
+    });
+
+    return NextResponse.json(product);
+  } catch (error) {
+    console.log("[PRODUCT_GET_ERROR]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string; productId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const {
+      name,
+      images,
+      price,
+      categoryId,
+      sizeId,
+      colorId,
+      isFeatured,
+      isArchived,
+    } = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!images || !images.length) {
+      return new NextResponse("Images are required", { status: 400 });
+    }
+
+    if (!price) {
+      return new NextResponse("Price is required", { status: 400 });
+    }
+
+    if (!categoryId) {
+      return new NextResponse("Category ID is required", { status: 400 });
+    }
+
+    if (!sizeId) {
+      return new NextResponse("Size ID is required", { status: 400 });
+    }
+
+    if (!colorId) {
+      return new NextResponse("Color ID is required", { status: 400 });
+    }
+
+    if (!params.productId) {
+      return new NextResponse("Product ID is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismaDb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    await prismaDb.product.update({
+      where: {
+        id: params.productId,
+      },
+      data: {
+        name,
+        price,
+        categoryId,
+        sizeId,
+        colorId,
+        isFeatured,
+        isArchived,
+        images: {
+          deleteMany: {},
+        },
+      },
+    });
+
+    const product = await prismaDb.product.update({
+      where: {
+        id: params.productId,
+      },
+      data: {
+        images: {
+          createMany: {
+            data: images.map((image: { url: string }) => image),
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(product);
+  } catch (error) {
+    console.log("[PRODUCT_PATCH_ERROR]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  _req: Request,
+  { params }: { params: { storeId: string; productId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    if (!params.productId) {
+      return new NextResponse("Product ID is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismaDb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const product = await prismaDb.product.deleteMany({
+      where: {
+        id: params.productId,
+      },
+    });
+
+    return NextResponse.json(product);
+  } catch (error) {
+    console.log("[PRODUCT_DELETE_ERROR]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
