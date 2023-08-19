5404e429c85e9f219ecc438b330e39f985284ea8
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-08-19 14:57:35 +0200

    format setup layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,30 +1,28 @@
-import { auth } from "@clerk/nextjs"
-import { redirect } from "next/navigation"
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-import prismaDb from "@/lib/prisma-db"
+import prismaDb from "@/lib/prisma-db";
 
-export default async function SetupLayout({ children }: {
-  children: React.ReactNode
+export default async function SetupLayout({
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-  const { userId } = auth()
+  const { userId } = auth();
 
   if (!userId) {
-    redirect('/sign-in')
+    redirect("/sign-in");
   }
 
   const store = await prismaDb.store.findFirst({
     where: {
-      userId
-    }
-  })
+      userId,
+    },
+  });
 
   if (store) {
-    redirect(`/${store.id}`)
+    redirect(`/${store.id}`);
   }
 
-  return (
-    <>
-      {children}
-    </>
-  )
+  return <>{children}</>;
 }
